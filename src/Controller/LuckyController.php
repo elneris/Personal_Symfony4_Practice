10758818552f4ee28ffbcbf2b70d25fca07d587c
Elneris Dang <elneris.dang@gmail.com>
2019-07-08 00:04:00 +0200

    Route et controller via annotation

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -5,9 +5,14 @@ namespace App\Controller;
 
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class LuckyController
 {
+    /**
+     * @Route("/lucky/number")
+     * @return Response
+     */
     public function number()
     {
         $number = rand(0, 100);
